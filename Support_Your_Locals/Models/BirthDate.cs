b71b29c4629f6d11248021710b14300bf062ae37
Evaldas <vievaldas@gmail.com>
2020-10-27 15:44:05 +0200

    Add date constructors for user and work.

---
+++ Support_Your_Locals/Models/BirthDate.cs
@@ -0,0 +1,21 @@
+﻿namespace Support_Your_Locals.Models
+{
+    public class BirthDate
+    {
+        public string Year { get; set; }
+        public string Month { get; set; }
+        public string Day { get; set; }
+
+        public BirthDate(string year, string month, string day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        public override string ToString()
+        {
+            return Year + "-" + Month + "-" + Day;
+        }
+    }
+}
---
+++ Support_Your_Locals/Models/WorkDate.cs
@@ -0,0 +1,20 @@
+﻿namespace Support_Your_Locals.Models
+{
+    public class WorkDate
+    {
+
+        public string Hours { get; set; }
+        public string Minutes { get; set; }
+
+        public WorkDate(string hours, string minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public override string ToString()
+        {
+            return Hours + ":" + Minutes;
+        }
+    }
+}
