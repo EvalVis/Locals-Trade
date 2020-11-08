4ac60dfc221d3972865b9eaa3af64402e89febde
Evaldas <vievaldas@gmail.com>
2020-11-08 13:08:36 +0200

    Centralize day names to infrastructure.

--- Support_Your_Locals/Models/ViewModels/TimeSheetRegisterViewModel.cs
+++ Support_Your_Locals/Models/ViewModels/TimeSheetRegisterViewModel.cs
@@ -4,7 +4,6 @@ namespace Support_Your_Locals.Models.ViewModels
 {
     public class TimeSheetRegisterViewModel
     {
-        public static string[] Days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
         public DateTime From { get; set; }
         public DateTime To { get; set; }
         public int Weekday { get; set; }
