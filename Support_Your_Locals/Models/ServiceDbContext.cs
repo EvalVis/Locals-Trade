fe8662febcdd0f108f4a67de7065cb218c49984a
Evaldas <vievaldas@gmail.com>
2020-11-10 11:43:53 +0200

    Setting Product in db context.

--- Support_Your_Locals/Models/ServiceDbContext.cs
+++ Support_Your_Locals/Models/ServiceDbContext.cs
@@ -16,6 +16,7 @@ namespace Support_Your_Locals.Models
         public DbSet<User> Users { get; set; }
         public DbSet<Business> Business { get; set; }
         public DbSet<TimeSheet> TimeSheets { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -25,6 +26,8 @@ namespace Support_Your_Locals.Models
                 WithMany(b => b.Workdays).HasForeignKey(t => t.BusinessID).IsRequired().OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<Business>().HasOne(b => b.User).WithMany(u => u.Businesses).HasForeignKey(b => b.UserID)
                 .IsRequired().OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Product>().HasOne(p => p.Business).WithMany(b => b.Products)
+                .HasForeignKey(p => p.BusinessID).IsRequired().OnDelete(DeleteBehavior.Cascade);
             base.OnModelCreating(modelBuilder);
         }
 
