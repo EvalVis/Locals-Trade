a87b0732b54324b98312b300524f12d4121a245b
Evaldas <vievaldas@gmail.com>
2020-11-10 11:41:19 +0200

    Product model.

---
+++ Support_Your_Locals/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Support_Your_Locals.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public string Unit { get; set; }
+        public string Comment { get; set; }
+        public long BusinessID { get; set; }
+        public Business Business { get; set; }
+    }
+}
