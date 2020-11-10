877acc914e29465150302b7cfcd8be54cec2f6c8
Evaldas <vievaldas@gmail.com>
2020-11-10 11:45:30 +0200

    Changing product to Product in Business.

--- Support_Your_Locals/Models/Business.cs
+++ Support_Your_Locals/Models/Business.cs
@@ -8,7 +8,6 @@ namespace Support_Your_Locals.Models
         public long BusinessID { get; set; }
         public long UserID { get; set; }
         public User User { get; set; }
-        public string Product { get; set; }
         public string Description { get; set; }
         public string Longitude { get; set; }
         public string Latitude { get; set; }
@@ -16,5 +15,6 @@ namespace Support_Your_Locals.Models
         public string Header { get; set; }
         public List<TimeSheet> Workdays { get; set; } = new List<TimeSheet>();
         public IList<string> Pictures { get; set; } = new List<string>();
+        public List<Product> Products { get; set; } = new List<Product>();
     }
 }
