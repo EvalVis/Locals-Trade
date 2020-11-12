4af39276552b3e69be7db1b6b00450a43644dd14
Evaldas <vievaldas@gmail.com>
2020-11-12 19:21:07 +0200

    Add picture to products.

--- Support_Your_Locals/Models/Product.cs
+++ Support_Your_Locals/Models/Product.cs
@@ -8,6 +8,7 @@
         public decimal PricePerUnit { get; set; }
         public string Unit { get; set; }
         public string Comment { get; set; }
+        public string Picture { get; set; }
         public long BusinessID { get; set; }
         public Business Business { get; set; }
     }
