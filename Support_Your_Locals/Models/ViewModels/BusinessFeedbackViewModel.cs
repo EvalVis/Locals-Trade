aa72369b397e1d1f13cd0a5f3ea0d32a66b1206c
Evaldas <vievaldas@gmail.com>
2020-11-15 16:00:57 +0200

    Combine business and feedback to a view model.

---
+++ Support_Your_Locals/Models/ViewModels/BusinessFeedbackViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Support_Your_Locals.Models.ViewModels
+{
+    public class BusinessFeedbackViewModel
+    {
+        public Business Business { get; set; }
+        public Feedback Feedback { get; set; }
+    }
+}
