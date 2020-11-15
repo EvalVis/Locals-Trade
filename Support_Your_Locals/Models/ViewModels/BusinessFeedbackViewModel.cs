be858c9471148bc0ade9dbabf67ea3a7742dda53
Evaldas <vievaldas@gmail.com>
2020-11-15 16:21:13 +0200

    Revert "Combine business and feedback to a view model."
    This reverts commit aa72369b397e1d1f13cd0a5f3ea0d32a66b1206c.

--- Support_Your_Locals/Models/ViewModels/BusinessFeedbackViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Support_Your_Locals.Models.ViewModels
-{
-    public class BusinessFeedbackViewModel
-    {
-        public Business Business { get; set; }
-        public Feedback Feedback { get; set; }
-    }
-}
