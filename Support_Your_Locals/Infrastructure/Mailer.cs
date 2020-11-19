f97d1b96a7690c05316c773f53b08619bac0a35f
Evaldas <vievaldas@gmail.com>
2020-11-19 16:49:35 +0200

    Anonymous methods in events.

--- Support_Your_Locals/Infrastructure/Mailer.cs
+++ Support_Your_Locals/Infrastructure/Mailer.cs
@@ -18,68 +18,73 @@ namespace Support_Your_Locals.Infrastructure
 
         private IServiceRepository repository;
         private IConfiguration config;
+        private BusinessController.FeedbackHandler handler;
 
         public Mailer(IApplicationBuilder app, IConfiguration configuration)
         {
             repository = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IServiceRepository>();
             config = configuration;
-            BusinessController.FeedbackEvent += SendMail;
+            handler = delegate(Feedback feedback)
+            {
+                MailMessage message = new MailMessage();
+                message.Subject = "LocalsTrade: Business feedback";
+                message.Body = $"Hello. You have received a new business feedback: \"{feedback.Text}\", from {feedback.SenderName}";
+                message.IsBodyHtml = false;
+
+                message.From = new MailAddress("localstradebox@gmail.com", "Locals Trade box");
+                string toEmail;
+                try
+                {
+                    toEmail = repository.Business.Where(b => b.BusinessID == feedback.BusinessID).Include(b => b.User)
+                        .First().User.Email;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Could not send email: Business somehow does not have an owner or " +
+                                    $"{nameof(User.Email)} field is null. Business ID: {feedback.BusinessID}. Detailed exception: {e}");
+                    return;
+                }
+
+                message.To.Add(new MailAddress(toEmail, toEmail));
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+
+                NetworkCredential credentials = new NetworkCredential("localstradebox@gmail.com", config["MailPassword"]);
+                smtp.UseDefaultCredentials = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = credentials;
+                smtp.Port = 587;
+                try
+                {
+                    //smtp.Send(message);
+                    smtp.SendAsync(message, null);
+                }
+                catch (SmtpFailedRecipientException e)
+                {
+                    Debug.WriteLine($"Failed to send an email (name of the sender: {feedback.SenderName}, message \"{feedback.Text}\") to a recipient {toEmail}. " +
+                                    $"Exception code: {e.StatusCode}. Detailed exception info: {e}");
+                }
+                catch (SmtpException e)
+                {
+                    Debug.WriteLine($"Failed to send email with smtp. The name of the sender: " +
+                                    $" {feedback.SenderName}. The message: \"{feedback.Text}\" was being sent to {toEmail}. " +
+                                    $"Exception code: {e.StatusCode}. " +
+                                    $"Detailed exception info: {e}");
+                }
+            };
+            BusinessController.FeedbackEvent += handler;
         }
 
         public void SendMail(Feedback feedback)
         {
-            MailMessage message = new MailMessage();
-            message.Subject = "LocalsTrade: Business feedback";
-            message.Body = $"Hello. You have received a new business feedback: \"{feedback.Text}\", from {feedback.SenderName}";
-            message.IsBodyHtml = false;
-
-            message.From = new MailAddress("localstradebox@gmail.com", "Locals Trade box");
-            string toEmail;
-            try
-            {
-                toEmail = repository.Business.Where(b => b.BusinessID == feedback.BusinessID).Include(b => b.User)
-                    .First().User.Email;
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine($"Could not send email: Business somehow does not have an owner or " +
-                                $"{nameof(User.Email)} field is null. Business ID: {feedback.BusinessID}. Detailed exception: {e}");
-                return;
-            }
-
-            message.To.Add(new MailAddress(toEmail, toEmail));
-
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "smtp.gmail.com";
-            smtp.EnableSsl = true;
-
-            NetworkCredential credentials = new NetworkCredential("localstradebox@gmail.com", config["MailPassword"]);
-            smtp.UseDefaultCredentials = true;
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtp.Credentials = credentials;
-            smtp.Port = 587;
-            try
-            {
-               //smtp.Send(message);
-                smtp.SendAsync(message, null);
-            }
-            catch (SmtpFailedRecipientException e)
-            {
-                Debug.WriteLine($"Failed to send an email (name of the sender: {feedback.SenderName}, message \"{feedback.Text}\") to a recipient {toEmail}. " +
-                                $"Exception code: {e.StatusCode}. Detailed exception info: {e}");
-            }
-            catch (SmtpException e)
-            {
-                Debug.WriteLine($"Failed to send email with smtp. The name of the sender: " +
-                                $" {feedback.SenderName}. The message: \"{feedback.Text}\" was being sent to {toEmail}. " +
-                                $"Exception code: {e.StatusCode}. " +
-                                $"Detailed exception info: {e}" );
-            }
+            
         }
 
         public void Mute()
         {
-            BusinessController.FeedbackEvent -= SendMail;
+            BusinessController.FeedbackEvent -= handler;
         }
 
     }
