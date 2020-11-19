87c85e8426ed08686aba44302b2b20c7fa46bf0f
Evaldas <vievaldas@gmail.com>
2020-11-19 22:27:21 +0200

    Asynchronious mailing with exceptions.

--- Support_Your_Locals/Infrastructure/Mailer.cs
+++ Support_Your_Locals/Infrastructure/Mailer.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -62,23 +63,25 @@ namespace Support_Your_Locals.Infrastructure
                 message.To.Add(new MailAddress(toEmail, toEmail));
 
                 SmtpClient protocol = smtp.Value;
-                try
-                {
-                    protocol.Send(message);
-                    //protocol.SendAsync(message, null);
-                }
-                catch (SmtpFailedRecipientException e)
-                {
-                    Debug.WriteLine($"Failed to send an email (name of the sender: {feedback.SenderName}, message \"{feedback.Text}\") to a recipient {toEmail}. " +
-                                    $"Exception code: {e.StatusCode}. Detailed exception info: {e}");
-                }
-                catch (SmtpException e)
-                {
-                    Debug.WriteLine($"Failed to send email with smtp. The name of the sender: " +
-                                    $" {feedback.SenderName}. The message: \"{feedback.Text}\" was being sent to {toEmail}. " +
-                                    $"Exception code: {e.StatusCode}. " +
-                                    $"Detailed exception info: {e}");
-                }
+                Task.Run( () =>
+                    {
+                        try
+                        {
+                            protocol.Send(message);
+                        }
+                        catch (SmtpFailedRecipientException e)
+                        {
+                            Debug.WriteLine($"Failed to send an email (name of the sender: {feedback.SenderName}, message \"{feedback.Text}\") to a recipient {toEmail}. " +
+                                            $"Exception code: {e.StatusCode}. Detailed exception info: {e}");
+                        }
+                        catch (SmtpException e)
+                        {
+                            Debug.WriteLine($"Failed to send email with smtp. The name of the sender: " +
+                                            $" {feedback.SenderName}. The message: \"{feedback.Text}\" was being sent to {toEmail}. " +
+                                            $"Exception code: {e.StatusCode}. " +
+                                            $"Detailed exception info: {e}");
+                        }
+                    });
             };
             BusinessController.FeedbackEvent += handler;
         }
