e2872a6f34ba6b92d0548d61cf1c63cf57eed934
Evaldas <vievaldas@gmail.com>
2020-12-05 21:28:04 +0200

    InsecureHandler.

--- MSupportYourLocals/MSupportYourLocals/Services/BusinessService.cs
+++ MSupportYourLocals/MSupportYourLocals/Services/BusinessService.cs
@@ -14,25 +14,39 @@ namespace MSupportYourLocals.Services
 
         public async Task<ObservableCollection<Business>> GetBusinesses()
         {
-            using (HttpClient httpClient = new HttpClient())
+            #if DEBUG
+            HttpClientHandler insecureHandler = GetInsecureHandler();
+            HttpClient httpClient = new HttpClient(insecureHandler);
+            #else
+            HttpClient client = new HttpClient();
+            #endif
+            httpClient.BaseAddress = new Uri("https://10.0.2.2:44311/"); 
+            httpClient.DefaultRequestHeaders.Accept.Clear(); 
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
+            System.Diagnostics.Debug.WriteLine("Testavimas 3"); 
+            HttpResponseMessage response = await httpClient.GetAsync("/api/Business/All"); 
+            System.Diagnostics.Debug.WriteLine("Testavimas 4"); 
+            if (response.StatusCode == HttpStatusCode.OK) 
+            { 
+                System.Diagnostics.Debug.WriteLine("Testavimas 5");
+                var result = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine("Testavimas 6");
+                ObservableCollection<Business> businesses = JsonConvert.DeserializeObject<ObservableCollection<Business>>(result);
+                return businesses;
+            } 
+            return null;
+        }
+
+        public HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
             {
-                httpClient.BaseAddress = new Uri("https://10.0.2.2:44311");
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                System.Diagnostics.Debug.WriteLine("Testavimas 3");
-                HttpResponseMessage response = await httpClient.GetAsync("/api/Business/All");
-                System.Diagnostics.Debug.WriteLine("Testavimas 4");
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    System.Diagnostics.Debug.WriteLine("Testavimas 5");
-                    var result = await response.Content.ReadAsStringAsync();
-                    System.Diagnostics.Debug.WriteLine("Testavimas 6");
-                    ObservableCollection<Business> businesses =
-                        JsonConvert.DeserializeObject<ObservableCollection<Business>>(result);
-                    return businesses;
-                }
-                return null;
-            }
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
         }
 
     }
