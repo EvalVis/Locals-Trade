c58dca4eb42f5b0df3baec68dff487b6c956568b
Evaldas <vievaldas@gmail.com>
2020-12-05 13:49:58 +0200

    New start.

---
+++ MSupportYourLocals/MSupportYourLocals/Models/BusinessItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MSupportYourLocals.Models
+{
+    class BusinessItem
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Header { get; set; }
+    }
+}
