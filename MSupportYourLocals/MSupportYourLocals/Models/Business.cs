094c31d957cc5cde40c5fb1789e28d05e4fd1a83
Evaldas <vievaldas@gmail.com>
2020-12-07 11:12:07 +0200

    Rendering owners.

--- MSupportYourLocals/MSupportYourLocals/Models/Business.cs
+++ MSupportYourLocals/MSupportYourLocals/Models/Business.cs
@@ -3,6 +3,7 @@
     public class Business
     {
         public long Id { get; set; }
+        public User User { get; set; }
         public string Description { get; set; }
         public string Longitude { get; set; }
         public string Latitude { get; set; }
