7656bd19e610264095d502126e7ec48b1e58e11e
Evaldas <vievaldas@gmail.com>
2020-12-07 21:05:38 +0200

    Logging in with UI.

--- MSupportYourLocals/MSupportYourLocals/MainPage.xaml.cs
+++ MSupportYourLocals/MSupportYourLocals/MainPage.xaml.cs
@@ -12,6 +12,12 @@ namespace MSupportYourLocals {
             InitializeComponent();
         }
 
+        public async void Login(Object sender, EventArgs e)
+        {
+            await loginService.Login(EmailEntry.Text, PasswordEntry.Text);
+            //await Navigation.PushAsync(new BusinessesView());
+        }
+
         public async void Skipped(Object sender, EventArgs e)
         {
             await Navigation.PushAsync(new BusinessesView());
