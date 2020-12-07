5fd3c0d6e20e8b0a34124af637c82bc3636baaae
Evaldas <vievaldas@gmail.com>
2020-12-07 21:10:47 +0200

    User Register service.

---
+++ MSupportYourLocals/MSupportYourLocals/Services/IRegisterUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+
+namespace MSupportYourLocals.Services
+{
+    public interface IRegisterUserService
+    {
+        Task Register(User user);
+    }
+}
---
+++ MSupportYourLocals/MSupportYourLocals/Services/RegisterUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+
+namespace MSupportYourLocals.Services
+{
+    public class RegisterUserService : Service, IRegisterUserService
+    {
+
+        public async Task Register(User user)
+        {
+            HttpClient httpClient = MakeHttpClient();
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync("/api/User/SignUp", user);
+        }
+
+    }
+}
