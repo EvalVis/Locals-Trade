111180a1ebc11e848bf99442e615f1925f709c26
Evaldas <vievaldas@gmail.com>
2020-12-08 20:47:57 +0200

    Feedback view.

---
+++ MSupportYourLocals/MSupportYourLocals/Views/FeedbackView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MSupportYourLocals.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FeedbackView : ContentPage
+    {
+        public FeedbackView()
+        {
+            InitializeComponent();
+        }
+    }
+}
