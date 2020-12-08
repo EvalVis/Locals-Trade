91e568a608f02b4f5b0a16eefae943b8f38d16e4
Evaldas <vievaldas@gmail.com>
2020-12-08 18:51:46 +0200

    Starting UserBusinessService template.

---
+++ MSupportYourLocals/MSupportYourLocals/Services/UserBusinessesService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+
+namespace MSupportYourLocals.Services
+{
+    public class UserBusinessesService : Service, IBusinessService
+    {
+
+        public Task<ObservableCollection<Business>> GetBusinesses()
+        {
+            HttpClient httpClient = MakeHttpClient();
+            //HttpResponseMessage response = await httpClient.GetAsync()
+        }
+
+    }
+}
