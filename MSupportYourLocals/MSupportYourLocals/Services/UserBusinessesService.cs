fb788a8285674eadd94159ec807c84344a6452da
Evaldas <vievaldas@gmail.com>
2020-12-08 19:02:35 +0200

    User Business Service.

--- MSupportYourLocals/MSupportYourLocals/Services/UserBusinessesService.cs
+++ MSupportYourLocals/MSupportYourLocals/Services/UserBusinessesService.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections.ObjectModel;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using MSupportYourLocals.Infrastructure;
 using MSupportYourLocals.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace MSupportYourLocals.Services
 {
     public class UserBusinessesService : Service, IBusinessService
     {
 
-        public Task<ObservableCollection<Business>> GetBusinesses()
+        private JsonWebTokenHolder tokenService = DependencyService.Get<JsonWebTokenHolder>();
+
+        public async Task<ObservableCollection<Business>> GetBusinesses()
         {
             HttpClient httpClient = MakeHttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync()
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenService.Token);
+            HttpResponseMessage response = await httpClient.GetAsync("/api/Business/User");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                ObservableCollection<Business> businesses = JsonConvert.DeserializeObject<ObservableCollection<Business>>(result);
+                businesses = SortByWeekday.Sort(businesses);
+                return businesses;
+            }
+            return null;
         }
 
     }
