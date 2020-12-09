2b612fc30cc816b5045a2e64a5fb50c6fa65586b
Evaldas <vievaldas@gmail.com>
2020-12-09 17:02:33 +0200

    UserBusinessViewModel.

---
+++ MSupportYourLocals/MSupportYourLocals/ViewModels/UserBusinessViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+using MSupportYourLocals.Services;
+using Xamarin.Forms;
+
+namespace MSupportYourLocals.ViewModels
+{
+    class UserBusinessViewModel
+    {
+
+        private IBusinessService businessService = DependencyService.Get<IBusinessService>();
+
+        private ObservableCollection<Business> businesses;
+
+        public UserBusinessViewModel()
+        {
+            Task.Run(async () => await GetBusinesses()).Wait();
+        }
+
+        public async Task GetBusinesses()
+        {
+            businesses = await businessService.GetUserBusinesses();
+        }
+    }
+}
