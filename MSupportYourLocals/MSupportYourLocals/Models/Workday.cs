a87ba6051bf0f5b125793e874fa61b77460c2e17
Evaldas <vievaldas@gmail.com>
2020-12-10 16:45:18 +0200

    Showing work hours and day normally.

--- MSupportYourLocals/MSupportYourLocals/Models/Workday.cs
+++ MSupportYourLocals/MSupportYourLocals/Models/Workday.cs
@@ -6,14 +6,11 @@ namespace MSupportYourLocals.Models
     public class Workday
     {
         public DateTime From { get; set; }
-        public string FromTime => $"{From.Hour}:{From.Minute}";
+        public string FromTime => $"{From.TimeOfDay}";
         public DateTime To { get; set; }
-        public string ToTime => $"{To.Hour}:{To.Minute}";
+        public string ToTime => $"{To.TimeOfDay}";
         public int Weekday { get; set; }
-        public string WeekdayName()
-        {
-            if(Weekday > 0 && Weekday < 8) return DayNames.Days[Weekday];
-            return null;
-        }
+        public string WeekdayName => DayNames.Days[Weekday - 1];
+
     }
 }
