cd848703107e68c78758a807231706f98037aad1
Evaldas <vievaldas@gmail.com>
2020-12-10 17:52:06 +0200

    User service.

---
+++ MSupportYourLocals/MSupportYourLocals/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+
+namespace MSupportYourLocals.Services
+{
+    public interface IUserService
+    {
+        Task<User> GetUser();
+    }
+}
---
+++ MSupportYourLocals/MSupportYourLocals/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using MSupportYourLocals.Models;
+using Newtonsoft.Json;
+
+namespace MSupportYourLocals.Services
+{
+    public class UserService : Service, IUserService
+    {
+
+        public async Task<User> GetUser()
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenService.Token);
+            HttpResponseMessage response = await httpClient.GetAsync("/api/User/Current");
+            if (response.StatusCode == HttpStatusCode.OK)
+            { 
+                var result = await response.Content.ReadAsStringAsync(); 
+                User user = JsonConvert.DeserializeObject<User>(result);
+                return user;
+            }
+            return null;
+        }
+
+    }
+}
