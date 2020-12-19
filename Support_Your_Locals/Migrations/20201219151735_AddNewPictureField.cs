ceae1386825cb56dea38d3254ed1e2b2aa5ea15d
MartynaUb <martub.mu@gmail.com>
2020-12-19 17:20:59 +0200

    picture fix

---
+++ Support_Your_Locals/Migrations/20201219151735_AddNewPictureField.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Support_Your_Locals.Migrations
+{
+    public partial class AddNewPictureField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Business");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PictureData",
+                table: "Business",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureData",
+                table: "Business");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "Business",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
