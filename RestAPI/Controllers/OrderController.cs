5f44d7b8b5b7933785a85d3860e319f2ea05ca3f
Evaldas <vievaldas@gmail.com>
2020-12-28 15:28:23 +0200

    Mailer.

---
+++ RestAPI/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestAPI.Models.Repositories;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController
+    {
+
+        private IServiceRepository repository;
+
+        public OrderController(IServiceRepository repo)
+        {
+            repository = repo;
+        }
+
+
+    }
+}
