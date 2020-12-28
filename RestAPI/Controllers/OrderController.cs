aa099cddb2d9ed0afe930d12813d996ed3d7f3f0
Evaldas <vievaldas@gmail.com>
2020-12-28 15:48:11 +0200

    Sending emails to potential buyer.

--- RestAPI/Controllers/OrderController.cs
+++ RestAPI/Controllers/OrderController.cs
@@ -1,18 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RestAPI.Infrastructure;
+using RestAPI.Models;
 using RestAPI.Models.Repositories;
 
 namespace RestAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class OrderController
+    public class OrderController : ControllerBase
     {
 
         private IServiceRepository repository;
+        private IConfiguration configuration;
 
-        public OrderController(IServiceRepository repo)
+        public OrderController(IServiceRepository repo, IConfiguration config)
         {
             repository = repo;
+        }
+
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public async Task<IActionResult> SuggestDelivery(DeliverySuggestion deliverySuggestion)
+        {
+            if(deliverySuggestion == null || deliverySuggestion.OrderId < 1)
+            {
+                return BadRequest();
+            }
+            Order order = await repository.Orders.Include(o => o.User).FirstOrDefaultAsync(o => o.Id == deliverySuggestion.OrderId);
+            if(order != null)
+            {
+                User user = order.User;
+                if(user != null)
+                {
+                    new Mailer(configuration).SendMail(deliverySuggestion.letter, user.Email);
+                }
+                return NotFound();
+            }
+            else
+            {
+                return NotFound();
+            }
+           // new Mailer(configuration).
+            //return Ok();
         }
 
 
---
+++ RestAPI/Models/DeliverySuggestion.cs
@@ -0,0 +1,8 @@
+﻿namespace RestAPI.Models
+{
+    public class DeliverySuggestion
+    {
+        public string letter { get; set; }
+        public long OrderId { get; set; }
+    }
+}
