21fe3b8887262f0d43436a5c5eebc91ebb2f5ba4
Evaldas <vievaldas@gmail.com>
2020-12-30 15:25:18 +0200

    Route controller.

---
+++ RestAPI/Controllers/RouteController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Models;
+using RestAPI.Models.Search;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpGet("products/{id}")]
+        public ActionResult ShortestRouteForProducts([FromQuery] OptimalRoute engine)
+        {
+            if(engine.ProductNames.Count > 5)
+            {
+                return BadRequest("Product list limit exceeded.");
+            }
+            List<Business> best = engine.FindBusinesses();
+            if(!best.Any())
+            {
+                return NoContent();
+            }
+            return Ok(best);
+        }
+
+    }
+}
