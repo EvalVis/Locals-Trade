b7a28dab548146ad5029c967ce3ce055581e609c
Evaldas <vievaldas@gmail.com>
2020-12-30 12:44:53 +0200

    Preventing null for product picture.

--- Support_Your_Locals/Models/Product.cs
+++ Support_Your_Locals/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Support_Your_Locals.Models
@@ -11,7 +12,14 @@ namespace Support_Your_Locals.Models
         public string Unit { get; set; }
         public string Comment { get; set; }
         [NotMapped]
-        public string Picture { get; set; }
+        public string Picture 
+        {
+            get
+            {
+                string imageBase64Data = Convert.ToBase64String(PictureData ?? new byte[1]);
+                return string.Format("data:image/jpg;base64,{0}", imageBase64Data);
+            }
+        }
         public byte[] PictureData { get; set; }
         public long BusinessID { get; set; }
         public Business Business { get; set; }
