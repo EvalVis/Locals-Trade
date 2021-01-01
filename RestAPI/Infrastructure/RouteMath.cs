d71f6ea28cadbbf04ec570aba78477890bc429f5
Evaldas <vievaldas@gmail.com>
2021-01-01 18:48:07 +0200

    Abstraction.

---
+++ RestAPI/Infrastructure/RouteMath.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestAPI.Models;
+
+namespace RestAPI.Infrastructure
+{
+    public class RouteMath
+    {
+
+        public static double ToRadians(double deg)
+        {
+            return (deg * Math.PI) / 180;
+        }
+
+        public static double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            double radLat1 = ToRadians(lat1);
+            double radLon1 = ToRadians(lon1);
+            double radLat2 = ToRadians(lat2);
+            double radLon2 = ToRadians(lon2);
+            double difLon = radLon1 - radLon2;
+            double difLat = radLat1 - radLat2;
+            double calc = Math.Pow(Math.Sin(difLat / 2), 2) + Math.Cos(radLat2) * Math.Cos(radLat1) * Math.Pow(Math.Sin(difLon / 2), 2);
+            double calculated = 2 * Math.Asin(Math.Sqrt(calc));
+            double r = 6371;
+            return calculated * r;
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+
+        public static async Task<Geocode> AddressToCoordinates(string address)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://api.positionstack.com/v1/forward");
+            HttpResponseMessage response = await client.GetAsync($"?access_key=92989511fc17d3b311e63a098337a457&query={address}");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                List<Geocode> data = JsonConvert.DeserializeObject<List<Geocode>>(result);
+                if (data.Count > 0)
+                {
+                    return data[0];
+                }
+            }
+            return null;
+        }
+
+    }
+}
--- RestAPI/Models/Search/OptimalCourierRoute.cs
+++ RestAPI/Models/Search/OptimalCourierRoute.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
+using RestAPI.Infrastructure;
 using RestAPI.Models.Repositories;
 
 namespace RestAPI.Models.Search
@@ -27,7 +25,7 @@ namespace RestAPI.Models.Search
             foreach (var order in orders)
             {
                 CourierTask orderTask = new CourierTask { isBusiness = false, Object = order };
-                orderTask.Geocode = await AddressToCoordinates(order.Address);
+                orderTask.Geocode = await RouteMath.AddressToCoordinates(order.Address);
                 Business b = repository.Business.FirstOrDefault(b => b.Products.Any(p => p.Orders.Any(o => o.Id == order.Id)));
                 if (b != null && b.Latitude != null && b.Longitude != null && !business.Contains(b))
                 {
@@ -78,49 +76,13 @@ namespace RestAPI.Models.Search
                 {
                     double cLat = t.Geocode.Latitude;
                     double cLon = t.Geocode.Longitude;
-                    distance += CalculateDistance(lastLat, lastLon, cLat, cLon);
+                    distance += RouteMath.CalculateDistance(lastLat, lastLon, cLat, cLon);
                     lastLat = cLat;
                     lastLon = cLon;
                 }
             }
-            distance += CalculateDistance(lastLat, lastLon, DestinationLatitude, DestinationLongitude);
+            distance += RouteMath.CalculateDistance(lastLat, lastLon, DestinationLatitude, DestinationLongitude);
             return distance;
-        }
-
-        private async Task<Geocode> AddressToCoordinates(string address)
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://api.positionstack.com/v1/forward");
-            HttpResponseMessage response = await client.GetAsync($"?access_key=92989511fc17d3b311e63a098337a457&query={address}");
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                var result = await response.Content.ReadAsStringAsync();
-                List<Geocode> data = JsonConvert.DeserializeObject<List<Geocode>>(result);
-                if(data.Count > 0)
-                {
-                    return data[0];
-                }
-            }
-            return null;
-        }
-
-        private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
-        {
-            double radLat1 = ToRadians(lon1);
-            double radLon1 = ToRadians(lat1);
-            double radLat2 = ToRadians(lat2);
-            double radLon2 = ToRadians(lon2);
-            double difLon = radLon1 - radLon2;
-            double difLat = radLat1 - radLat2;
-            double calc = Math.Pow(Math.Sin(difLat / 2), 2) + Math.Cos(radLat2) * Math.Cos(radLat1) * Math.Pow(Math.Sin(difLon / 2), 2);
-            double calculated = 2 * Math.Asin(Math.Sqrt(calc));
-            double r = 6371;
-            return calculated * r;
-        }
-
-        private double ToRadians(double deg)
-        {
-            return (deg * Math.PI) / 180;
         }
 
         private IEnumerable<IEnumerable<CourierTask>> GetPermutations(IEnumerable<CourierTask> list, int length)
